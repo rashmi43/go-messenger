6244dda568fba2d8cc297b43707ae1ffe3dcd2d4
rashmi43 <k.rashmi.r@gmail.com>
2020-08-10 18:09:24 +0530

    add crud with controller

---
+++ mapstore/mapstore.go
@@ -0,0 +1,61 @@
+package mapstore
+
+import (
+	"github.com/go-messenger/domain"
+	"errors"
+)
+
+type MapStore struct {
+	store map[string]domain.Message // An in-memory store with a map
+}
+
+func NewMapStore() *MapStore {
+	return &MapStore { store: make(map[string]domain.Message)}
+}
+
+func (m MapStore) Create(c domain.Message)(error) {
+	messageID := c.ID
+	if _,ok := m.store[messageID]; ok {
+		return errors.New("The message id already exists in store")
+	}
+	m.store[messageID] = c
+	return nil
+}
+
+func (m MapStore) Update( newid string, c domain.Message )(error) {
+	messageID := newid
+	if _,ok := m.store[messageID]; ok {
+		return errors.New("The message with the given ID doesn't exist")
+	}
+	m.store[messageID] = c
+	return nil
+}
+
+func (m MapStore) Delete(id string)(error) {
+	messageID := id
+	if _,ok := m.store[messageID]; !ok {
+		return errors.New("The message with the given ID doesn't exist")
+	}
+	delete(m.store, messageID)
+	return nil
+
+}
+
+func (m MapStore) GetAll()([]domain.Message, error) {
+	messageList := []domain.Message{}
+	for _, v := range(m.store) {
+		messageList = append(messageList,v)
+	}
+	return messageList, nil
+
+}
+
+func (m MapStore) GetById(id string) (domain.Message, error){
+	messageID := id
+	if message,ok := m.store[messageID]; !ok {
+		return domain.Message{}, errors.New("The message with the given ID doesn't exist")
+	} else{
+		return message, nil
+	}
+
+}
---
+++ router/router.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"github.com/gorilla/mux"
+	c "github.com/go-messenger/controller"
+
+)
+
+
+// SetMessageRoutes registers routes for messages
+func SetMessageRoutes(messageController *c.MessageController) *mux.Router {
+	messageRouter := mux.NewRouter()
+
+	messageRouter.Handle("/v1/messages", c.ResponseHandler(messageController.Post)).Methods("POST")
+	messageRouter.Handle("/v1/messages", c.ResponseHandler(messageController.Get)).Methods("GET")
+	messageRouter.Handle("/v1/messages/{id}", c.ResponseHandler(messageController.Put)).Methods("PUT")
+	messageRouter.Handle("/v1/messages{id}", c.ResponseHandler(messageController.Delete)).Methods("DELETE")
+	messageRouter.Handle("/v1/messages/{id}", c.ResponseHandler(messageController.GetById)).Methods("GET")
+	// Applying authorization middleware
+	//router.PathPrefix("/v1/messages").Handler(auth.AuthorizeRequest(messageRouter))
+	return messageRouter
+}
